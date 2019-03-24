6b56d35891520d5f66d3089bfd60f699efdbedda
Marcin Radoszewski <moriturius@gmail.com>
2019-03-24 16:07:30 +0100

    Added hackernews client and periodical items fetching

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,24 +1,30 @@
-use std::error;
-
 use rocket::State;
 use rocket_contrib::json::Json;
 use rocket_contrib::serve::StaticFiles;
 use rocket_cors::AllowedOrigins;
 
 use crate::calendar::Event;
+use crate::clients::hackernews::Item;
 use crate::common::*;
+use std::sync::{Arc, RwLock};
 
 pub struct Api {}
 
 #[get("/events")]
-fn events(app_state: State<SharedAppState>) -> Result<Json<Vec<Event>>, Box<error::Error>> {
+fn events(app_state: State<SharedAppState>) -> Json<Vec<Event>> {
     // TODO: zamiast error::Error powinien pewnie zwracać jakiś Json<RestError>
     let app_state = app_state.read().unwrap();
-    Ok(Json(app_state.events.clone()))
+    Json(app_state.events.clone())
+}
+
+#[get("/feed")]
+fn feed(app_state: State<SharedAppState>) -> Json<Vec<Item>> {
+    let app_state = app_state.read().unwrap();
+    Json(app_state.feed.clone())
 }
 
 impl Api {
-    pub fn run_server(app_state: Shared<AppState>) {
+    pub fn run_server(app_state: Arc<RwLock<AppState>>) {
         let options = rocket_cors::Cors {
             allowed_origins: AllowedOrigins::all(),
             ..Default::default()
@@ -27,7 +33,7 @@ impl Api {
         rocket::ignite()
             .manage(app_state)
             .mount("/static/", StaticFiles::from("static/"))
-            .mount("/", routes![events])
+            .mount("/", routes![events, feed])
             .attach(options)
             .launch();
     }
